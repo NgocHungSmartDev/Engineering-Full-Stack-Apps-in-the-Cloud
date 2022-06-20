2c7257693dc8cab5fa32927769ea31a86893dc8f
NgocHungSmartDev <hoang.ngoc.hungit@gmail.com>
2022-06-20 17:00:07 +0700

    fix bug delete img

--- image-filter-starter-code/src/util/util.ts
+++ image-filter-starter-code/src/util/util.ts
@@ -13,13 +13,14 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
     try {
       const photo = await Jimp.read(inputURL);
       const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+        "/tmp/";
+        const nameImg = "filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
-        .write(__dirname + outpath, (img) => {
-          resolve(__dirname + outpath);
+        .write(__dirname + outpath +nameImg, (img) => {
+          resolve(nameImg);
         });
     } catch (error) {
       reject(error);
